65db75a73f486421fa2bbc746f9c5fa04e9bf29d
taylorshab <tshabani9@gmail.com>
2016-12-06 11:12:43 -0800

    just because.

--- DestroyingtheWorld/ViewController.swift
+++ DestroyingtheWorld/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         print ("Hello World")
+        
+        print ("please work. thx")
     }
 
     override func didReceiveMemoryWarning() {
